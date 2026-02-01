c3acbb1a352a89f2342a264fef812297a9d722f5
Jakub <jwf.adamczyk@gmail.com>
2026-02-01 05:54:54 +0100

    Add sample program demonstrating Sharpex runtime usage

---
+++ Sharpex.Playground/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Sharpex.Playground;
+
+internal static class Program
+{
+    private static int money = 15;
+    
+    private static void Main(string[] args)
+    {
+        Sharpex.Eval("#test");
+        Sharpex.Eval("#pay 10");
+        Sharpex.Eval("#pay 10");
+    }
+    
+    
+    [Sharpex("test")]
+    private static bool Test()
+    {
+        Console.WriteLine("Hello Sharpex!");
+        return true;
+    }
+    
+    [Sharpex("pay")]
+    private static bool Pay(int amount)
+    {
+        if (amount > money)
+        {
+            Console.WriteLine("You don't have enough money.");
+            return false;
+        }
+        money -= amount;
+        Console.WriteLine("You paid " + amount + " USD.");
+        return true;
+    }
+    
+    
+}
